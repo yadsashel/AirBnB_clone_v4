4aad9dbe9071d40ab77fa0ca20074b8f0693c676
yadsashel <ysalhi600@gmail.com>
2024-08-10 07:26:20 -0700

    0-hbnb.py done

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flask Web Application """
-import uuid
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -8,10 +7,8 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-
+import uuid
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -32,11 +29,15 @@ def hbnb():
 
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
-
+    cache_id = str(uuid.uuid4())
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html', cache_id=uuid.uuid4())
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
